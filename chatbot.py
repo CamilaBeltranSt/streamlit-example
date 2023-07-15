2017b8f030296213266f52e9d8449335dbd218b4
CamilaBeltranSt <camilabeltranst@gmail.com>
2023-07-15 15:22:40 -0400

    w

--- chatbot.py
+++ chatbot.py
@@ -1,15 +1,15 @@
 import streamlit as st
 from PIL import Image
+import requests
 
 def generate_response(input_text):
-    # Aquí puedes agregar la lógica para generar la respuesta del modelo GPT
-    # Basado en el texto de entrada
-    # Puedes utilizar tu propio modelo o una API de chatbot como OpenAI GPT-3
+    url='http://127.0.0.1:8000/predict'
 
-    # Ejemplo de respuesta estática
-    response = "Hola, soy un chatbot. ¿En qué puedo ayudarte?"
+    params= {'user_query':input_text
+         }
+    response=requests.get(url=url,params=params).json()
 
-    return response
+    return response['answer_query']['Respuesta']['content']
 
 def main():
     # Configuración de la página
@@ -24,39 +24,36 @@ def main():
     st.write("Welcome! This is a chatbot created by the FunesBot team at Data Science 1203 Le Wagon. He can answer your questions about a book. Please enter your questions in the text field below.")
 
     # Lista para almacenar el historial de preguntas y respuestas
-    chat_history = []
+    messages = []
 
     # Bucle continuo de preguntas y respuestas
     while True:
-        # Área de texto para que el usuario ingrese la pregunta
+         # Área de texto para que el usuario ingrese la pregunta
         user_input = st.text_input("Enter your question here:")
 
         # Verificar si se presiona la tecla Enter en el teclado
-        if user_input and (st.button("Enviar") or st.session_state.enter_pressed):
+        if user_input and (st.button("Send") or st.session_state.enter_pressed):
             # Generar respuesta
             response = generate_response(user_input)
 
             # Agregar la pregunta y respuesta al historial
-            chat_history.append((user_input, response))
+            messages.append((user_input, response))
 
             # Restablecer el valor de enter_pressed
             st.session_state.enter_pressed = False
 
         # Mostrar el historial de preguntas y respuestas
-        with st.expander("Historial completo"):
-            for i, (question, answer) in enumerate(chat_history, 1):
-                st.write(f"**Pregunta {i}:** {question}")
-                st.write(f"**Respuesta {i}:** {answer}")
+        container = st.container()
+        for i, (question, answer) in enumerate(messages, 1):
+                st.write(f"**Question {i}:** {question}")
+                st.write(f"**Answer {i}:** {answer}")
 
         # Mostrar la respuesta más reciente
-        if len(chat_history) > 0:
+        if len(messages) > 0:
             st.subheader("Respuesta más reciente:")
-            st.text(chat_history[-1][1])
-
-        # Opción para hacer otra pregunta
-        another_question = st.button("Make other question")
-        if not another_question:
-            break
-
+            st.text(messages[-1][1])
+        
+        break
+    
 if __name__ == "__main__":
     main()
